ee23297636e9594f15ba9a3a6368d1f0ecba24e9
Harshad Hegde <hegdehb@gmail.com>
2023-10-27 12:49:41 -0500

    formatted

--- kg_bacdive/utils/pandas_utils.py
+++ kg_bacdive/utils/pandas_utils.py
@@ -20,22 +20,23 @@ def drop_duplicates(file_path: Path):
 
 
 def establish_transitive_relationship(
-        file_path: Path,
-        subject_prefix: str,
-        intermediate_prefix: str,
-        predicate: str,
-        object_prefix: str
-    ) -> pd.DataFrame:
-    """Establish transitive relationship given the predicate is the same.
+    file_path: Path,
+    subject_prefix: str,
+    intermediate_prefix: str,
+    predicate: str,
+    object_prefix: str,
+) -> pd.DataFrame:
+    """
+    Establish transitive relationship given the predicate is the same.
 
-    e.g.: Existant relations:
+    e.g.: Existent relations:
         1. A => predicate => B
         2. B => predicate => C
-    
+
     This function adds the relation A => predicate => C
 
 
-    :param file_path: Filepath of thes edge file.
+    :param file_path: Filepath of the edge file.
     :param subject_prefix: Subject prefix (A in the example)
     :param intermediate_prefix: Intermediate prefix that connects the subject to object (B in the example).
     :param predicate: The common predicate between all relations.
@@ -45,7 +46,9 @@ def establish_transitive_relationship(
     df = drop_duplicates(file_path)
     df_relations = df.loc[df[PREDICATE_COLUMN] == predicate]
     subject_condition = df_relations[SUBJECT_COLUMN].str.startswith(subject_prefix)
-    intermediate_subject_condition = df_relations[SUBJECT_COLUMN].str.startswith(intermediate_prefix)
+    intermediate_subject_condition = df_relations[SUBJECT_COLUMN].str.startswith(
+        intermediate_prefix
+    )
     object_condition = df_relations[OBJECT_COLUMN].str.startswith(object_prefix)
     intermediate_object_condition = df_relations[OBJECT_COLUMN].str.startswith(intermediate_prefix)
     subject_intermediate_df = df_relations[subject_condition & intermediate_object_condition]
@@ -54,8 +57,12 @@ def establish_transitive_relationship(
     list_of_dfs_to_append = []
 
     for row in subject_intermediate_df.iterrows():
-        transitive_relations_df = intermediate_object_df.loc[intermediate_object_df[SUBJECT_COLUMN] == row[1].object]
-        transitive_relations_df.loc[transitive_relations_df[SUBJECT_COLUMN] == row[1].object, SUBJECT_COLUMN] = row[1].subject
+        transitive_relations_df = intermediate_object_df.loc[
+            intermediate_object_df[SUBJECT_COLUMN] == row[1].object
+        ]
+        transitive_relations_df.loc[
+            transitive_relations_df[SUBJECT_COLUMN] == row[1].object, SUBJECT_COLUMN
+        ] = row[1].subject
         list_of_dfs_to_append.append(transitive_relations_df)
 
     df = pd.concat([df] + list_of_dfs_to_append)
